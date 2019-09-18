7e8999a17bd7f6e64a55ce6bcb500ca07a417b39
Nick Haidari <haidari@bc.edu>
2019-09-18 17:20:41 -0400

    4.2 completed, bounce animation added on image press

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //mark: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
     
@@ -17,8 +17,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    //Mark: functions
+    func animateImage(){
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        //shrink the image to punch by 60pixls
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds}, completion: nil)
+    }
+    //Mark: actions
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
     
@@ -26,7 +34,7 @@ class ViewController: UIViewController {
     @IBAction func cameraPressed(_ sender: UIButton) {
     }
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-    print("Pressed Image")
+        animateImage()
     
     }
 }
